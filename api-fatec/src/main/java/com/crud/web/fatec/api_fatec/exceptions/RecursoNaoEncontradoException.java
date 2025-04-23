26a87b9107846b29e437e6b33f234a4559a10d92
pedrohenriquebarcellos <pedrostyle@gmail.com>
2025-04-23 19:15:42 -0300

    :construction: feat: create Custom Exceptions

---
+++ api-fatec/src/main/java/com/crud/web/fatec/api_fatec/exceptions/RecursoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.crud.web.fatec.api_fatec.exceptions;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
